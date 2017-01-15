760d8c4aa54b07014a8218cc41d433fe5d27ca3c
benjaminboruff <ben@boruff.me>
2017-01-15 12:54:42 -0500

    basic keypad reorg

--- src/components/key.js
+++ src/components/key.js
@@ -3,10 +3,14 @@ import 'react-mdl/extra/material.css';
 import 'react-mdl/extra/material.js';
 import { Button } from 'react-mdl';
 import React from 'react';
+//import './key.css';
 
 function Key (props: Object) {
-    let symbol = props.value;
-    switch (symbol) {
+  let symbol;
+  // a <Key value="1"/> component must have a value prop
+  // that matches a known type
+  if(props.value) {
+    switch (props.value) {
       case '+':
         symbol = '\uFF0B';
         break;
@@ -19,29 +23,105 @@ function Key (props: Object) {
       case '/':
         symbol = '\uFF0F';
         break;
+      case '=':
+        symbol = '\uFF1D';
+        break;
+      case 'blank':
+        symbol = '0';
+        break;
       default:
+        // key is a number or CLEAR/UNDO
+        symbol = props.value;
         break;
     }
 
-    if(props.accent) {
-      return(
-        <Button raised accent
-          style={props.style}
-          value={props.value}
-          onClick={props.handleClick}>
-          {symbol}
-        </Button>
-      );
-    }
+    // CLEAR and UNDO keys
+      if(props.value === 'CLEAR' || props.value === 'UNDO') {
+        return(
+          <Button
+            id={props.value.toLowerCase()}
+            // bsStyle="danger"
+            style={props.style}
+            value={props.value}
+            onClick={props.handleClick}>
+            {symbol}
+          </Button>
+        );
+      }
 
-    return(
-      <Button raised
-        style={props.style}
-        value={props.value}
-        onClick={props.handleClick}>
-        {symbol}
-      </Button>
-    );
+      // equals key
+      if(props.value === "=") {
+        return(
+          <Button
+            id="equals"
+            bsSize="large"
+            // bsStyle="success"
+            value={props.value}
+            onClick={props.handleClick}>
+            {symbol}
+          </Button>
+        );
+      }
+
+      // numbers and decimal
+      if(props.value.match(/[0-9\.]/)) {
+        return(
+          <Button
+            className="numops"
+            // bsStyle="primary"
+            value={props.value}
+            onClick={props.handleClick}>
+            {symbol}
+          </Button>
+        );
+      }
+
+      // operators
+      if(props.value.match(/[\+\-\*\/]/)) {
+        return(
+          <Button
+            className="numops"
+            // bsStyle="primary"
+            value={props.value}
+            onClick={props.handleClick}>
+            {symbol}
+          </Button>
+        );
+      }
+
+      // the blank button
+      if(props.value === 'blank') {
+        return(
+          <Button
+            disabled
+            id="blank"
+            value={props.value}
+            onClick={props.handleClick}>
+            <span>o</span>
+          </Button>
+        );
+      }
+
+    // else the value prop was not of the correct type
+    return <Button>WRONG VALUE</Button>;
+
+  } else {
+    // no value prop was found
+    return <Button>NO VALUE</Button>;
+  }
+
+  // MDL specific
+    // if(props.accent) {
+    //   return(
+    //     <Button raised accent
+    //       style={props.style}
+    //       value={props.value}
+    //       onClick={props.handleClick}>
+    //       {symbol}
+    //     </Button>
+    //   );
+    // }
+
 }
 
 export default Key;
--- src/components/keypad.js
+++ src/components/keypad.js
@@ -6,9 +6,55 @@ import React from 'react';
 import Key from './key';
 
 function Keypad(props: Object) {
+  const row1 = ['1','2','3','+'];
+  const row2 = ['4','5','6','-'];
+  const row3 = ['7','8','9','*'];
+  const row4 = ['0','.','blank','/'];
+
   const numbers = ['1','2','3','4','5','6','7','8','9','0','.'];
   const operators = ["+", "-", "*", "/"];
   const clear = ["CLEAR", "UNDO"];
+
+  let row1KeySet = row1.map((item) => {
+    return (
+      <Key
+        key={item}
+        value={item}
+        handleClick={props.handleButtonClick}
+      />
+    );
+  });
+
+  let row2KeySet = row2.map((item) => {
+    return (
+      <Key
+        key={item}
+        value={item}
+        handleClick={props.handleButtonClick}
+      />
+    );
+  });
+
+  let row3KeySet = row3.map((item) => {
+    return (
+      <Key
+        key={item}
+        value={item}
+        handleClick={props.handleButtonClick}
+      />
+    );
+  });
+
+  let row4KeySet = row4.map((item) => {
+    return (
+      <Key
+        key={item}
+        value={item}
+        handleClick={props.handleButtonClick}
+      />
+    );
+  });
+
   let numberKeySet = numbers.map((number) => {
     return (
       <Key
@@ -35,9 +81,10 @@ function Keypad(props: Object) {
   });
   return (
     <div>
-      {numberKeySet}
-      <br />
-      {operatorKeySet}
+      {row1KeySet}
+      {row2KeySet}
+      {row3KeySet}
+      {row4KeySet}
       <br />
       <Button raised ripple style={{margin: '4px 1px 1px', width: '268px', fontWeight: '900', backgroundColor: '#4caf50', color: '#fff'}} onClick={props.calculate}>{'\uFF1D'}</Button>
       <br />
